e98afeba97fd80ce9c8d1e32f37c51e41cd08e1c
avasaf <114611531+avasaf@users.noreply.github.com>
2025-09-08 14:04:55 +0200

    fix: expand popup graph to fill space

--- src/runtime/config.ts
+++ src/runtime/config.ts
@@ -29,8 +29,24 @@ export interface Config {
   precipitationBarColor: string
   maxPrecipitationColor: string
 
-  // UI
-  refreshIconColor: string
+  // UI buttons
+  refreshButtonBackgroundColor: string
+  refreshButtonIconColor: string
+  expandButtonBackgroundColor: string
+  expandButtonIconColor: string
+  expandButtonBorderRadius: number
+
+  // Popup
+  popupBackgroundColor: string
+  popupPadding: number
+  popupBorderRadius: number
+  popupBoxShadowOffsetX: number
+  popupBoxShadowOffsetY: number
+  popupBoxShadowBlur: number
+  popupBoxShadowSpread: number
+  popupBoxShadowColor: string
+  blockPage: boolean
+  maskColor: string
 }
 
 export type IMConfig = ImmutableObject<Config>
